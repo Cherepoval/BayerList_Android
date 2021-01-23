ad543924f61c6a662b205719f6bba19752fdc5c8
Cherepoval <alex.invicto@hotmail.com>
2021-01-23 15:44:08 +0000

    Toolbar title

--- app/src/main/java/com/example/byerlist/MainActivity.java
+++ app/src/main/java/com/example/byerlist/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ArrayAdapter<String> adapter;
     private AppBarConfiguration mAppBarConfiguration;
     private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +39,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,new ArrayList<String>(Arrays.asList(arr)));
         list.setAdapter(adapter);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.menu_meat);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -66,66 +68,77 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.nav_meat) {
+            toolbar.setTitle(R.string.menu_meat);
             arr = getResources().getStringArray(R.array.meat);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
         }
         else if (id == R.id.nav_bread) {
+            toolbar.setTitle(R.string.menu_bread);
             arr = getResources().getStringArray(R.array.bread);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_fish) {
+            toolbar.setTitle(R.string.menu_fish);
             arr = getResources().getStringArray(R.array.fish);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_fruit) {
+            toolbar.setTitle(R.string.menu_fruit);
             arr = getResources().getStringArray(R.array.fruits);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_dessert) {
+            toolbar.setTitle(R.string.menu_dessert);
             arr = getResources().getStringArray(R.array.dessert);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_gastr) {
+            toolbar.setTitle(R.string.menu_gastr);
             arr = getResources().getStringArray(R.array.gastronomy);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_grocery) {
+            toolbar.setTitle(R.string.menu_grocery);
             arr = getResources().getStringArray(R.array.grocery);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_water) {
+            toolbar.setTitle(R.string.menu_water);
             arr = getResources().getStringArray(R.array.drink);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_milk) {
+            toolbar.setTitle(R.string.menu_milk);
             arr = getResources().getStringArray(R.array.milk);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_rice) {
+            toolbar.setTitle(R.string.menu_rice);
             arr = getResources().getStringArray(R.array.groats_pasta);
             adapter.clear();
             adapter.addAll(arr);
             adapter.notifyDataSetChanged();
 
         }else if (id == R.id.nav_spices) {
+            toolbar.setTitle(R.string.menu_spices);
             arr = getResources().getStringArray(R.array.spices);
             adapter.clear();
             adapter.addAll(arr);
